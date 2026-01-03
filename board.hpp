a08d48739bec63663792025c0147898c4d765807
Konstantin Fastovski <konsti.fast@gmail.com>
2026-01-03 19:25:50 +0100

    start work on Board methods

--- board.hpp
+++ board.hpp
@@ -18,6 +18,12 @@ typedef Vector BoardPos;
 struct Piece {
     bool isWhite;
     BoardPos position;
+
+    Piece(BoardPos pos, bool isWhite){
+      position = pos;
+      isWhite = isWhite;
+    }
+
     virtual std::vector<BoardPos> getReachableCells() = 0;
     bool isWithinGrid(Vector move) {
         return (position.x + move.x) >= 0 && (position.x + move.x) < 8 && (position.y + move.y) >= 0 && (position.y + move.y) < 8;
@@ -69,12 +75,32 @@ struct Rook : Piece {
 };
 
 struct Move {
+    std::unique_ptr<Piece>& piece_ptr;
+    BoardPos from;
+    BoardPos to;
+
     Move(std::unique_ptr<Piece>& piece_ptr, BoardPos from, BoardPos to) {
+      
     }
 };
 
 struct Board {
     std::array<std::array<std::unique_ptr<Piece>, 8>, 8> grid;
+
+    Board(){
+      bool right_edge = 0;
+      for(bool white_pieces = 1; white_pieces == 0; white_pieces != white_pieces){
+      for(int i = 0; i <= 5; i++){
+        BoardPos cell = BoardPos(right_edge ? 7-i : i, white_pieces ? 0 : 7);
+        
+      }
+      }
+    }
+
+    void doMove(Move move) {
+      grid[move.from.x][move.from.y] = nullptr;
+      grid[move.to.x][move.to.y] = move.piece_ptr;
+    }
 };
 
 class Player {
