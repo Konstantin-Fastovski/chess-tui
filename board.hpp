38bfd71bb195c4e6f9cc5640331cf0e810380300
Placeblock <felix@weglehner.info>
2026-01-05 03:27:24 +0100

    Fixed some bugs Added basic game logic

--- board.hpp
+++ board.hpp
@@ -14,9 +14,30 @@ struct Vector {
     Vector operator*(const int8_t& factor) const {
         return {static_cast<int8_t>(x * factor), static_cast<int8_t>(y * factor)};
     }
-
+    Vector& operator*=(const int8_t& factor) {
+        this->x = static_cast<int8_t>(this->x * factor);
+        this->y = static_cast<int8_t>(this->y * factor);
+        return *this;
+    }
     Vector operator+(const Vector& summand) const {
         return {static_cast<int8_t>(x + summand.x), static_cast<int8_t>(y + summand.y)};
+    }
+    Vector &operator+=(const Vector & vector) {
+        this->x = static_cast<int8_t>(this->x + vector.x);
+        this->y = static_cast<int8_t>(this->y + vector.y);
+        return *this;
+    }
+    Vector operator-(const Vector& summand) const {
+        return {static_cast<int8_t>(x + summand.x), static_cast<int8_t>(y + summand.y)};
+    }
+    Vector &operator-=(const Vector & vector) {
+        this->x = static_cast<int8_t>(this->x - vector.x);
+        this->y = static_cast<int8_t>(this->y - vector.y);
+        return *this;
+    }
+
+    bool operator==(const Vector& other) const {
+        return x == other.x && y == other.y;
     }
 
     Vector mirrorHorizontal() const {
@@ -34,6 +55,11 @@ struct Vector {
     Vector* getAllPossibleTransforms() {
         //TODO
     }
+
+    [[nodiscard]] bool isWithinGrid() const {
+        return this->x >= 0 && this->x < 8 && this->y >= 0 && this->y < 8;
+    }
+
 };
 
 typedef Vector BoardPos;
@@ -50,78 +76,73 @@ inline BoardPos parseBoardPos(const std::string &input) {
     if (rank < 48 || rank > 56) { // Some Unicode magic numbers
         throw std::invalid_argument("invalid rank symbol");
     }
-    return {static_cast<uint8_t>(rank-48), static_cast<uint8_t>(file-97)};
+    return {static_cast<int8_t>(8-(rank-48)), static_cast<int8_t>(file-97)};
 }
 
 struct Piece {
     bool white;
-    BoardPos position;
 
-    Piece(const BoardPos pos, const bool white)
-        : white(white), position(pos) {
+    Piece(const bool white)
+        : white(white) {
     }
 
-    virtual std::vector<BoardPos> getReachableCells() = 0;
+    virtual std::vector<BoardPos> getReachableCells(const BoardPos &position) = 0;
     virtual std::string getUnicode() = 0;
-
-    bool isWithinGrid(const Vector& move) const {
-        return (position.x + move.x) >= 0 && (position.x + move.x) < 8 && (position.y + move.y) >= 0 && (position.y + move.y) < 8;
-    }
 
     virtual ~Piece() = default;
 };
 
 struct Pawn final : Piece {
-    Pawn(const BoardPos &pos, const bool white)
-        : Piece(pos, white) {
+    Pawn(const bool white)
+        : Piece(white) {
     }
 
     const int8_t dir = white ? 1 : -1;
-    const uint8_t start_y = white ? 1 : 6;
+    const uint8_t start_x = white ? 1 : 6;
 
-    std::vector<BoardPos> getReachableCells() override {
+    std::vector<BoardPos> getReachableCells(const BoardPos &position) override {
         std::vector<BoardPos> reachable_cells;
 
-        const Vector base_move = Vector(0, 1);
+        const Vector base_move = Vector(1, 0);
 
-        if (position.y == start_y) {
+        if (position.x == start_x) {
             reachable_cells.emplace_back(position + base_move * 2 * dir); //why emplace_back?
         }
         
         reachable_cells.push_back(position + base_move * dir); //withinBoard checks unnecessary cuz pawns can't be on the last rank lol
         return reachable_cells;
     }
-    std::vector<BoardPos> getCapturableCells() const {
+    std::vector<BoardPos> getCapturableCells(const BoardPos &position) const {
         std::vector<BoardPos> capturable_cells;
         const int8_t dir = white ? 1 : -1;
         auto destination = BoardPos(position.x - 1, position.y + 1 * dir);
-        if (isWithinGrid(destination)) {
+        if (destination.isWithinGrid()) {
             capturable_cells.push_back(destination);
         }
         destination = BoardPos(position.x + 1, position.y + 1 * dir);
-        if (isWithinGrid(destination)) {
+        if (destination.isWithinGrid()) {
             capturable_cells.push_back(destination);
         }
         return capturable_cells;
     }
 
     std::string getUnicode() override {
-        return "\u265F";
+        return this->white ? "\u2659" : "\u265F";
     }
 };
 
 struct Rook final : Piece {
-    Rook(const BoardPos &pos, const bool white)
-        : Piece(pos, white) {
+    Rook(const bool white)
+        : Piece(white) {
     }
 
-    std::vector<BoardPos> getReachableCells() override {
+    std::vector<BoardPos> getReachableCells(const BoardPos &position) override {
         std::vector<BoardPos> reachable_cells;
         const Vector base_move = Vector(1, 0);
         for (const Vector& rotated : {base_move, base_move.rotate90(true), base_move.mirrorHorizontal(), base_move.rotate90(false)}) {
             for (int i = 1; ; i++) {
                 BoardPos destination = position + rotated * i;
-                if (!isWithinGrid(destination)) break;
+                if (!destination.isWithinGrid()) break;
                 reachable_cells.push_back(destination);
             }
         }
@@ -129,22 +150,22 @@ struct Rook final : Piece {
     }
 
     std::string getUnicode() override {
-        return "\u2656";
+        return this->white ? "\u2656" : "\u265C";
     }
 };
 
 struct Knight final : Piece {
-    Knight(const BoardPos &pos, const bool white)
-        : Piece(pos, white) {
+    Knight(const bool white)
+        : Piece(white) {
     }
 
-    std::vector<BoardPos> getReachableCells() override {
+    std::vector<BoardPos> getReachableCells(const BoardPos &position) override {
         std::vector<BoardPos> reachable_cells;
         const Vector base_move = Vector(2, 1);
         for (const Vector& mirrored : {base_move, base_move.mirrorHorizontal(), base_move.mirrorVertical(), base_move.mirrorHorizontal().mirrorVertical()}) {
             for (const Vector& rotated : {mirrored, mirrored.rotate90(true)}) {
                 const BoardPos destination = position + rotated;
-                if (isWithinGrid(destination)) {
+                if (destination.isWithinGrid()) {
                     reachable_cells.push_back(destination);
                 }
             }
@@ -153,53 +174,46 @@ struct Knight final : Piece {
     }
 
     std::string getUnicode() override {
-        return "";
+        return this->white ? "\u2658" : "\u265E";
     }
 };
 
 struct King final : Piece {
-    King(const BoardPos &pos, const bool white)
-        : Piece(pos, white) {
+    King(const bool white)
+        : Piece(white) {
     }
 
-    std::vector<BoardPos> getReachableCells() override {
+    std::vector<BoardPos> getReachableCells(const BoardPos &position) override {
         std::vector<BoardPos> reachable_cells;
-        Vector base_move = Vector(1, 1);
-        for (const Vector& mirrored : {base_move, base_move.mirrorHorizontal(), base_move.mirrorVertical(), base_move.mirrorHorizontal().mirrorVertical()}) {
-            const BoardPos destination = position + mirrored;
-            if (isWithinGrid(destination)) {
-                reachable_cells.push_back(destination);
-            }
-        }
-        base_move = Vector(1, 0);
-        for (const Vector& rotated : {base_move, base_move.rotate90(true), base_move.mirrorHorizontal(), base_move.rotate90(false)}) {
-            for (int i = 1; ; i++) {
-                BoardPos destination = position + rotated * i;
-                if (!isWithinGrid(destination)) break;
-                reachable_cells.push_back(destination);
+        for (int8_t x = -1; x < 1; ++x) {
+            for (int8_t y = -1; y < 1; ++y) {
+                BoardPos destination = position + Vector(x, y);
+                if (x != 0 && y != 0 && destination.isWithinGrid()) {
+                    reachable_cells.emplace_back(destination);
+                }
             }
         }
         return reachable_cells;
     }
 
     std::string getUnicode() override {
-        return "";
+        return this->white ? "\u2654" : "\u265A";
     }
 };
 
 struct Bishop final : Piece {
-    Bishop(const BoardPos &pos, const bool white)
-        : Piece(pos, white) {
+    Bishop(const bool white)
+        : Piece(white) {
     }
 
-    std::vector<BoardPos> getReachableCells() override {
+    std::vector<BoardPos> getReachableCells(const BoardPos &position) override {
         std::vector<BoardPos> reachable_cells;
 
         Vector base_move = Vector(1, 1);
         for (const Vector& mirrored : {base_move, base_move.mirrorHorizontal(), base_move.mirrorVertical(), base_move.mirrorHorizontal().mirrorVertical()}) {
             for (int i = 1; ; i++) {
                 BoardPos destination = position + mirrored * i;
-                if (!isWithinGrid(destination)) break;
+                if (!destination.isWithinGrid()) break;
                 reachable_cells.push_back(destination);
             }
         }
@@ -207,7 +221,34 @@ struct Bishop final : Piece {
     }
 
     std::string getUnicode() override {
-        return "";
+        return this->white ? "\u2657" : "\u265D";
+    }
+};
+
+struct Queen final : Piece {
+    explicit Queen(const bool white)
+        : Piece(white) {
+    }
+
+    std::vector<BoardPos> getReachableCells(const BoardPos &position) override {
+        std::vector<BoardPos> reachable_cells;
+        for (BoardPos dest = position; dest.isWithinGrid(); dest += Vector{1, 1}) {
+            reachable_cells.emplace_back(dest);
+        }
+        for (BoardPos dest = position; dest.isWithinGrid(); dest += Vector{-1, 1}) {
+            reachable_cells.emplace_back(dest);
+        }
+        for (BoardPos dest = position; dest.isWithinGrid(); dest += Vector{1, -1}) {
+            reachable_cells.emplace_back(dest);
+        }
+        for (BoardPos dest = position; dest.isWithinGrid(); dest += Vector{-1, -1}) {
+            reachable_cells.emplace_back(dest);
+        }
+        return reachable_cells;
+    }
+
+    std::string getUnicode() override {
+        return this->white ? "\u2655" : "\u265B";
     }
 };
 
@@ -223,25 +264,54 @@ struct Move {
 struct Board {
     std::array<std::array<std::unique_ptr<Piece>, 8>, 8> grid;
 
+    Board() {
+        for (int y = 0; y < 8; ++y) {
+            this->grid[1][y] = std::make_unique<Pawn>(true);
+            this->grid[6][y] = std::make_unique<Pawn>(false);
+        }
+        this->grid[0][0] = std::make_unique<Rook>(true);
+        this->grid[0][7] = std::make_unique<Rook>(true);
+        this->grid[7][0] = std::make_unique<Rook>(false);
+        this->grid[7][7] = std::make_unique<Rook>(false);
+        this->grid[0][1] = std::make_unique<Knight>(true);
+        this->grid[0][6] = std::make_unique<Knight>(true);
+        this->grid[7][1] = std::make_unique<Knight>(false);
+        this->grid[7][6] = std::make_unique<Knight>(false);
+        this->grid[0][2] = std::make_unique<Bishop>(true);
+        this->grid[0][5] = std::make_unique<Bishop>(true);
+        this->grid[7][2] = std::make_unique<Bishop>(false);
+        this->grid[7][5] = std::make_unique<Bishop>(false);
+        this->grid[0][3] = std::make_unique<Queen>(true);
+        this->grid[0][4] = std::make_unique<King>(true);
+        this->grid[7][3] = std::make_unique<King>(false);
+        this->grid[7][4] = std::make_unique<Queen>(false);
+    }
+
     // TODO: This method should return some kind of "MoveResult" which also states if a player won and so on
     void applyMove(const Move move) {
       grid[move.to.x][move.to.y] = std::move(grid[move.from.x][move.from.y]);
     }
 
     void draw() const {
-        std::cout << "┏━━━━━━━━━━━━━━━━━┓" << std::endl;
-        for (const auto& row : this->grid) {
-            std::cout << "┃";
-            for (const auto& piece : row) {
-                if (piece) {
+        std::cout << "┏━━━━━━━━━━━━━━━━━━━┓" << std::endl;
+        std::cout << "┃  a b c d e f g h  ┃" << std::endl;
+        for (int x = 0; x < 8; ++x) {
+            std::cout << "┃" << std::to_string(8-x);
+            for (int y = 0; y < 8; ++y) {
+                if (const auto &piece = this->grid[x][y]) {
                     std::cout << " " << piece->getUnicode();
                 } else {
-                    std::cout << "   ";
+                    std::cout << "  ";
                 }
             }
-            std::cout << " ┃" << std::endl;
+            std::cout << " " << std::to_string(8-x) << "┃" << std::endl;
         }
-        std::cout << "┗━━━━━━━━━━━━━━━━━┛" << std::endl;
+        std::cout << "┃  a b c d e f g h  ┃" << std::endl;
+        std::cout << "┗━━━━━━━━━━━━━━━━━━━┛" << std::endl;
+    }
+
+    std::unique_ptr<Piece> &getPiece(const BoardPos &pos) {
+        return this->grid[pos.x][pos.y];
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -11,12 +11,37 @@ int main() {
     Board board;
 
     while (true) {
+        board.draw();
+
         std::cout << "It's Player " << current_player+1 << "'s turn! " << std::endl;
         const auto move = players[current_player]->requestMove();
+        std::cout << "Requested Move: (" << std::to_string(move.from.x) << ", " << std::to_string(move.from.y) << ")"
+            << " > (" << std::to_string(move.to.x) << ", " << std::to_string(move.to.y) << ")" << std::endl;
 
-        // TODO: Validate move
+        const auto &piece = board.getPiece(move.from);
+        if (!piece) {
+            std::cout << "This square is empty" << std::endl;
+            continue;
+        }
+        if (piece->white != current_player) {
+            std::cout << "This is not your piece" << std::endl;
+        }
+        const std::vector<BoardPos> reachable_cells = piece->getReachableCells(move.from);
+        if (std::ranges::find(reachable_cells, move.to) == reachable_cells.end()) {
+            std::cout << "This is not a valid move" << std::endl;
+            continue;
+        }
+        auto &capturePiece = board.getPiece(move.to);
+        if (capturePiece) {
+            if (capturePiece->white == piece->white) {
+                std::cout << "You cannot capture your own piece" << std::endl;
+                continue;
+            }
+            std::cout << "You captured a " << capturePiece->getUnicode() << std::endl;
+        }
+        capturePiece.reset();
+
         board.applyMove(move);
-        board.draw();
 
         current_player = (current_player + 1) % 2;
     }
